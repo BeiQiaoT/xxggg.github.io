a0144deab6b35effc485650d084df590e4c79213
XXGGG <38970638+XXGGG@users.noreply.github.com>
2022-07-01 08:38:19 +0000

    Deploying to GithubPages from  @ 8b8d7238ab4830539af642984a6cb099622c902c 🚀

---
+++ assets/FrontEnd_CSS_layout_Center.md.1770cb38.js
@@ -0,0 +1,81 @@
+import{_ as s,c as n,o as a,a as l}from"./app.9049e128.js";const A=JSON.parse('{"title":"\u{1F7E5} \u300E\u5C45\u4E2D\u300F","description":"","frontmatter":{},"headers":[{"level":2,"title":"\u7EDD\u5BF9\u5B9A\u4F4D + \u4E0A\u4E0B\u5DE6\u53F3\u4E3A0","slug":"\u7EDD\u5BF9\u5B9A\u4F4D-\u4E0A\u4E0B\u5DE6\u53F3\u4E3A0"},{"level":2,"title":"\u7EDD\u5BF9\u5B9A\u4F4D + \u8D1F\u8FB9\u8DDD","slug":"\u7EDD\u5BF9\u5B9A\u4F4D-\u8D1F\u8FB9\u8DDD"},{"level":2,"title":"\u7EDD\u5BF9\u5B9A\u4F4D + \u5E73\u79FB","slug":"\u7EDD\u5BF9\u5B9A\u4F4D-\u5E73\u79FB"},{"level":2,"title":"Grid","slug":"grid"},{"level":2,"title":"Flex","slug":"flex"}],"relativePath":"FrontEnd/CSS/layout_Center.md","lastUpdated":1656664651000}'),p={name:"FrontEnd/CSS/layout_Center.md"},o=l(`<h1 id="\u{1F7E5}-\u300E\u5C45\u4E2D\u300F" tabindex="-1">\u{1F7E5} \u300E\u5C45\u4E2D\u300F <a class="header-anchor" href="#\u{1F7E5}-\u300E\u5C45\u4E2D\u300F" aria-hidden="true">#</a></h1><h2 id="\u7EDD\u5BF9\u5B9A\u4F4D-\u4E0A\u4E0B\u5DE6\u53F3\u4E3A0" tabindex="-1">\u7EDD\u5BF9\u5B9A\u4F4D + \u4E0A\u4E0B\u5DE6\u53F3\u4E3A0 <a class="header-anchor" href="#\u7EDD\u5BF9\u5B9A\u4F4D-\u4E0A\u4E0B\u5DE6\u53F3\u4E3A0" aria-hidden="true">#</a></h2><div class="language-css"><span class="copy"></span><pre><code><span class="line"><span style="color:#89DDFF;">.</span><span style="color:#FFCB6B;">class</span><span style="color:#89DDFF;">{</span></span>
+<span class="line"><span style="color:#A6ACCD;">	</span><span style="color:#B2CCD6;">position</span><span style="color:#89DDFF;">:</span><span style="color:#A6ACCD;"> absolute</span><span style="color:#89DDFF;">;</span></span>
+<span class="line"><span style="color:#A6ACCD;">	</span></span>
+<span class="line"><span style="color:#A6ACCD;">	</span><span style="color:#676E95;font-style:italic;">/* \u4E0A\u4E0B\u5DE6\u53F3\u5168\u90E8\u4E3A0 */</span></span>
+<span class="line"><span style="color:#A6ACCD;">	</span><span style="color:#B2CCD6;">top</span><span style="color:#89DDFF;">:</span><span style="color:#A6ACCD;"> </span><span style="color:#F78C6C;">0</span><span style="color:#89DDFF;">;</span></span>
+<span class="line"><span style="color:#A6ACCD;">	</span><span style="color:#B2CCD6;">right</span><span style="color:#89DDFF;">:</span><span style="color:#A6ACCD;"> </span><span style="color:#F78C6C;">0</span><span style="color:#89DDFF;">;</span></span>
+<span class="line"><span style="color:#A6ACCD;">	</span><span style="color:#B2CCD6;">bottom</span><span style="color:#89DDFF;">:</span><span style="color:#A6ACCD;"> </span><span style="color:#F78C6C;">0</span><span style="color:#89DDFF;">;</span></span>
+<span class="line"><span style="color:#A6ACCD;">	</span><span style="color:#B2CCD6;">left</span><span style="color:#89DDFF;">:</span><span style="color:#A6ACCD;"> </span><span style="color:#F78C6C;">0</span><span style="color:#89DDFF;">;</span></span>
+<span class="line"><span style="color:#A6ACCD;">	</span></span>
+<span class="line"><span style="color:#A6ACCD;">	</span><span style="color:#B2CCD6;">width</span><span style="color:#89DDFF;">:</span><span style="color:#A6ACCD;"> </span><span style="color:#F78C6C;">70%</span><span style="color:#89DDFF;">;</span></span>
+<span class="line"><span style="color:#A6ACCD;">  </span><span style="color:#B2CCD6;">height</span><span style="color:#89DDFF;">:</span><span style="color:#A6ACCD;"> </span><span style="color:#F78C6C;">25%</span><span style="color:#89DDFF;">;</span></span>
+<span class="line"><span style="color:#89DDFF;">}</span></span>
+<span class="line"></span></code></pre></div><h2 id="\u7EDD\u5BF9\u5B9A\u4F4D-\u8D1F\u8FB9\u8DDD" tabindex="-1">\u7EDD\u5BF9\u5B9A\u4F4D + \u8D1F\u8FB9\u8DDD <a class="header-anchor" href="#\u7EDD\u5BF9\u5B9A\u4F4D-\u8D1F\u8FB9\u8DDD" aria-hidden="true">#</a></h2><div class="language-css"><span class="copy"></span><pre><code><span class="line"><span style="color:#89DDFF;">.</span><span style="color:#FFCB6B;">center</span><span style="color:#A6ACCD;"> </span><span style="color:#89DDFF;">{</span></span>
+<span class="line"><span style="color:#A6ACCD;">      </span><span style="color:#676E95;font-style:italic;">/* \u7EDD\u5BF9\u5B9A\u4F4D */</span></span>
+<span class="line"><span style="color:#A6ACCD;">      </span><span style="color:#B2CCD6;">position</span><span style="color:#89DDFF;">:</span><span style="color:#A6ACCD;"> absolute</span><span style="color:#89DDFF;">;</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#A6ACCD;">      </span><span style="color:#676E95;font-style:italic;">/* \u4E0A\u65B9\u548C\u5DE6\u65B9\u4E3A50% */</span></span>
+<span class="line"><span style="color:#A6ACCD;">      </span><span style="color:#B2CCD6;">top</span><span style="color:#89DDFF;">:</span><span style="color:#A6ACCD;"> </span><span style="color:#F78C6C;">50%</span><span style="color:#89DDFF;">;</span></span>
+<span class="line"><span style="color:#A6ACCD;">      </span><span style="color:#B2CCD6;">left</span><span style="color:#89DDFF;">:</span><span style="color:#A6ACCD;"> </span><span style="color:#F78C6C;">50%</span><span style="color:#89DDFF;">;</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#A6ACCD;">      </span><span style="color:#676E95;font-style:italic;">/* \u7ED9\u5B9A\u5BBD\u9AD8 */</span></span>
+<span class="line"><span style="color:#A6ACCD;">      </span><span style="color:#B2CCD6;">width</span><span style="color:#89DDFF;">:</span><span style="color:#A6ACCD;"> </span><span style="color:#F78C6C;">300px</span><span style="color:#89DDFF;">;</span></span>
+<span class="line"><span style="color:#A6ACCD;">      </span><span style="color:#B2CCD6;">height</span><span style="color:#89DDFF;">:</span><span style="color:#A6ACCD;"> </span><span style="color:#F78C6C;">200px</span><span style="color:#89DDFF;">;</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#A6ACCD;">      </span><span style="color:#676E95;font-style:italic;">/* \u4E0A\u5916\u8FB9\u8DDD\u4E3A\u8D1F\u7684\u7ED9\u5B9A\u9AD8\u5EA6\u7684\u4E00\u534A */</span></span>
+<span class="line"><span style="color:#A6ACCD;">      </span><span style="color:#B2CCD6;">margin-top</span><span style="color:#89DDFF;">:</span><span style="color:#A6ACCD;"> </span><span style="color:#F78C6C;">-100px</span><span style="color:#89DDFF;">;</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#A6ACCD;">      </span><span style="color:#676E95;font-style:italic;">/* \u5DE6\u5916\u8FB9\u8DDD\u4E3A\u8D1F\u7684\u7ED9\u5B9A\u5BBD\u5EA6\u7684\u4E00\u534A */</span></span>
+<span class="line"><span style="color:#A6ACCD;">      </span><span style="color:#B2CCD6;">margin-left</span><span style="color:#89DDFF;">:</span><span style="color:#A6ACCD;"> </span><span style="color:#F78C6C;">-150px</span><span style="color:#89DDFF;">;</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#A6ACCD;">  </span><span style="color:#89DDFF;">}</span></span>
+<span class="line"></span></code></pre></div><h2 id="\u7EDD\u5BF9\u5B9A\u4F4D-\u5E73\u79FB" tabindex="-1">\u7EDD\u5BF9\u5B9A\u4F4D + \u5E73\u79FB <a class="header-anchor" href="#\u7EDD\u5BF9\u5B9A\u4F4D-\u5E73\u79FB" aria-hidden="true">#</a></h2><div class="language-css"><span class="copy"></span><pre><code><span class="line"><span style="color:#89DDFF;">.</span><span style="color:#FFCB6B;">center</span><span style="color:#A6ACCD;"> </span><span style="color:#89DDFF;">{</span></span>
+<span class="line"><span style="color:#A6ACCD;">	</span><span style="color:#676E95;font-style:italic;">/* \u7EDD\u5BF9\u5B9A\u4F4D */</span></span>
+<span class="line"><span style="color:#A6ACCD;">	</span><span style="color:#B2CCD6;">position</span><span style="color:#89DDFF;">:</span><span style="color:#A6ACCD;"> absolute</span><span style="color:#89DDFF;">;</span></span>
+<span class="line"><span style="color:#A6ACCD;">	</span></span>
+<span class="line"><span style="color:#A6ACCD;">	</span><span style="color:#676E95;font-style:italic;">/* \u4E0A\u65B9\u548C\u5DE6\u65B9\u4E3A50% */</span></span>
+<span class="line"><span style="color:#A6ACCD;">	</span><span style="color:#B2CCD6;">top</span><span style="color:#89DDFF;">:</span><span style="color:#A6ACCD;"> </span><span style="color:#F78C6C;">50%</span><span style="color:#89DDFF;">;</span></span>
+<span class="line"><span style="color:#A6ACCD;">	</span><span style="color:#B2CCD6;">left</span><span style="color:#89DDFF;">:</span><span style="color:#A6ACCD;"> </span><span style="color:#F78C6C;">50%</span><span style="color:#89DDFF;">;</span></span>
+<span class="line"><span style="color:#A6ACCD;">	</span></span>
+<span class="line"><span style="color:#A6ACCD;">	</span><span style="color:#676E95;font-style:italic;">/* \u4E0D\u7528\u7ED9\u5BBD\u9AD8\uFF0C\u4F46\u662F\u53EF\u4EE5\u7ED9\u4E2A\u5185\u8FB9\u8DDD\u9632\u6B62\u5185\u5BB9\u4E0E\u76D2\u5B50\u8FC7\u4E8E\u8D34\u5408 */</span></span>
+<span class="line"><span style="color:#A6ACCD;">	</span><span style="color:#B2CCD6;">padding</span><span style="color:#89DDFF;">:</span><span style="color:#A6ACCD;"> </span><span style="color:#F78C6C;">10px</span><span style="color:#89DDFF;">;</span></span>
+<span class="line"><span style="color:#A6ACCD;">	</span></span>
+<span class="line"><span style="color:#A6ACCD;">	</span><span style="color:#676E95;font-style:italic;">/* \u8FD9\u4E2A50%\u662F\u76F8\u5BF9\u4E8E\u81EA\u8EAB\u5BBD\u9AD8\u800C\u8A00\u7684 */</span></span>
+<span class="line"><span style="color:#A6ACCD;">	</span><span style="color:#B2CCD6;">transform</span><span style="color:#89DDFF;">:</span><span style="color:#A6ACCD;"> </span><span style="color:#82AAFF;">translate</span><span style="color:#89DDFF;">(</span><span style="color:#F78C6C;">-50%</span><span style="color:#89DDFF;">,</span><span style="color:#A6ACCD;"> </span><span style="color:#F78C6C;">-50%</span><span style="color:#89DDFF;">);</span></span>
+<span class="line"><span style="color:#A6ACCD;">	</span></span>
+<span class="line"><span style="color:#A6ACCD;">	</span><span style="color:#676E95;font-style:italic;">/* \u767D\u8272\u80CC\u666F */</span></span>
+<span class="line"><span style="color:#A6ACCD;">	</span><span style="color:#B2CCD6;">background</span><span style="color:#89DDFF;">:</span><span style="color:#A6ACCD;"> white</span><span style="color:#89DDFF;">;</span></span>
+<span class="line"><span style="color:#89DDFF;">}</span></span>
+<span class="line"></span></code></pre></div><h2 id="grid" tabindex="-1">Grid <a class="header-anchor" href="#grid" aria-hidden="true">#</a></h2><div class="language-css"><span class="copy"></span><pre><code><span class="line"><span style="color:#676E95;font-style:italic;">/* \u4E2D\u592E\u76D2\u5B50\u7684\u76F4\u63A5\u7236\u5143\u7D20 */</span></span>
+<span class="line"><span style="color:#FFCB6B;">body</span><span style="color:#A6ACCD;"> </span><span style="color:#89DDFF;">{</span></span>
+<span class="line"><span style="color:#A6ACCD;">	</span><span style="color:#676E95;font-style:italic;">/* \u4EE4\u5176\u53D8\u6210\u7F51\u683C\u5E03\u5C40 */</span></span>
+<span class="line"><span style="color:#A6ACCD;">	</span><span style="color:#B2CCD6;">display</span><span style="color:#89DDFF;">:</span><span style="color:#A6ACCD;"> grid</span><span style="color:#89DDFF;">;</span></span>
+<span class="line"><span style="color:#A6ACCD;">	</span></span>
+<span class="line"><span style="color:#A6ACCD;">	</span><span style="color:#676E95;font-style:italic;">/* \u4EE4\u5B50\u5143\u7D20\u5C45\u4E2D */</span></span>
+<span class="line"><span style="color:#A6ACCD;">	</span><span style="color:#B2CCD6;">place-items</span><span style="color:#89DDFF;">:</span><span style="color:#A6ACCD;"> center</span><span style="color:#89DDFF;">;</span></span>
+<span class="line"><span style="color:#89DDFF;">}</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#89DDFF;">.</span><span style="color:#FFCB6B;">center</span><span style="color:#A6ACCD;"> </span><span style="color:#89DDFF;">{</span></span>
+<span class="line"><span style="color:#A6ACCD;">	</span><span style="color:#676E95;font-style:italic;">/* \u4E0D\u7528\u7ED9\u5BBD\u9AD8\uFF0C\u4F46\u662F\u53EF\u4EE5\u7ED9\u4E2A\u5185\u8FB9\u8DDD\u9632\u6B62\u5185\u5BB9\u4E0E\u76D2\u5B50\u8FC7\u4E8E\u8D34\u5408 */</span></span>
+<span class="line"><span style="color:#A6ACCD;">	</span><span style="color:#B2CCD6;">padding</span><span style="color:#89DDFF;">:</span><span style="color:#A6ACCD;"> </span><span style="color:#F78C6C;">10px</span><span style="color:#89DDFF;">;</span></span>
+<span class="line"><span style="color:#A6ACCD;">	</span></span>
+<span class="line"><span style="color:#A6ACCD;">	</span><span style="color:#676E95;font-style:italic;">/* \u767D\u8272\u80CC\u666F */</span></span>
+<span class="line"><span style="color:#A6ACCD;">	</span><span style="color:#B2CCD6;">background</span><span style="color:#89DDFF;">:</span><span style="color:#A6ACCD;"> white</span><span style="color:#89DDFF;">;</span></span>
+<span class="line"><span style="color:#89DDFF;">}</span></span>
+<span class="line"></span></code></pre></div><h2 id="flex" tabindex="-1">Flex <a class="header-anchor" href="#flex" aria-hidden="true">#</a></h2><div class="language-css"><span class="copy"></span><pre><code><span class="line"><span style="color:#676E95;font-style:italic;">/* \u627E\u5230\u4E2D\u592E\u76D2\u5B50\u7684\u76F4\u63A5\u7236\u5143\u7D20 */</span></span>
+<span class="line"><span style="color:#FFCB6B;">body</span><span style="color:#A6ACCD;"> </span><span style="color:#89DDFF;">{</span></span>
+<span class="line"><span style="color:#A6ACCD;">	</span><span style="color:#676E95;font-style:italic;">/* \u4EE4\u5176\u53D8\u6210\u5F39\u6027\u5E03\u5C40 */</span></span>
+<span class="line"><span style="color:#A6ACCD;">	</span><span style="color:#B2CCD6;">display</span><span style="color:#89DDFF;">:</span><span style="color:#A6ACCD;"> flex</span><span style="color:#89DDFF;">;</span></span>
+<span class="line"><span style="color:#89DDFF;">}</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#89DDFF;">.</span><span style="color:#FFCB6B;">center</span><span style="color:#A6ACCD;"> </span><span style="color:#89DDFF;">{</span></span>
+<span class="line"><span style="color:#A6ACCD;">	</span><span style="color:#676E95;font-style:italic;">/* \u81EA\u52A8\u5916\u8FB9\u8DDD */</span></span>
+<span class="line"><span style="color:#A6ACCD;">	</span><span style="color:#B2CCD6;">margin</span><span style="color:#89DDFF;">:</span><span style="color:#A6ACCD;"> auto</span><span style="color:#89DDFF;">;</span></span>
+<span class="line"><span style="color:#A6ACCD;">	</span></span>
+<span class="line"><span style="color:#A6ACCD;">	</span><span style="color:#676E95;font-style:italic;">/* \u767D\u8272\u80CC\u666F */</span></span>
+<span class="line"><span style="color:#A6ACCD;">	</span><span style="color:#B2CCD6;">background</span><span style="color:#89DDFF;">:</span><span style="color:#A6ACCD;"> white</span><span style="color:#89DDFF;">;</span></span>
+<span class="line"><span style="color:#A6ACCD;">	</span></span>
+<span class="line"><span style="color:#A6ACCD;">	</span><span style="color:#676E95;font-style:italic;">/* \u4E0D\u7528\u7ED9\u5BBD\u9AD8\uFF0C\u4F46\u662F\u53EF\u4EE5\u7ED9\u4E2A\u5185\u8FB9\u8DDD\u9632\u6B62\u5185\u5BB9\u4E0E\u76D2\u5B50\u8FC7\u4E8E\u8D34\u5408 */</span></span>
+<span class="line"><span style="color:#A6ACCD;">	</span><span style="color:#B2CCD6;">padding</span><span style="color:#89DDFF;">:</span><span style="color:#A6ACCD;"> </span><span style="color:#F78C6C;">10px</span><span style="color:#89DDFF;">;</span></span>
+<span class="line"><span style="color:#89DDFF;">}</span></span>
+<span class="line"></span></code></pre></div>`,11),e=[o];function t(c,r,C,y,D,i){return a(),n("div",null,e)}var d=s(p,[["render",t]]);export{A as __pageData,d as default};
